ce93f125fb9cecf5a4509a465fae02ca587ceb9d
taurimenas <taurimenas01@gmail.com>
2020-11-25 22:39:16 +0200

    Cleaned code

--- Warehouse Management System/Program.cs
+++ Warehouse Management System/Program.cs
@@ -1,16 +1,10 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Events;
 using Serilog.Sinks.MSSqlServer;
-using Serilog.Sinks.MSSqlServer.Sinks.MSSqlServer.Options;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Warehouse_Management_System
 {
